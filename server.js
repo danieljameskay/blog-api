1e0c62f498b39d5dd591ed4806529ccfcc26c1f8
Daniel <danieljameskay@gmail.com>
2017-05-06 13:53:21 +0100

    Added cors stuff

--- server.js
+++ server.js
@@ -10,11 +10,18 @@ const express = require("express"),
 moongoose.connect(process.env.DB_URI);
 
 bodyParser.json();
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
+
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://danieljameskay.github.io/blog');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(express.static(__dirname + '/public'));
 app.use(expressLayouts);
