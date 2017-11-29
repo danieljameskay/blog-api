9708e5540188ed8a00f5040a847727d8af190248
Daniel Kay <danieljameskay@gmail.com>
2017-11-29 12:10:26 +0000

    Update server.js
    further changes....

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ const express = require("express"),
     bodyParser = require('body-parser');
     moongoose = require('mongoose');
 
-moongoose.connect(process.env.DB_URI);
+let port = process.env.PORT || 8000;
 
+moongoose.connect(process.env.DB_URI);
 bodyParser.json();
-app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
+app.use(expressLayouts);
+app.use(require('./app/routes'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(function (req, res, next) {
-
     res.setHeader('Access-Control-Allow-Origin', process.env.SITE_URI);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -22,10 +25,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use(expressLayouts);
-app.use(require('./app/routes'));
-
-app.listen(process.env.PORT || 8080, () => {
+app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 })
